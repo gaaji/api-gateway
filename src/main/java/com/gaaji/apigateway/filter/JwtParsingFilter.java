45d8245d59dbe9f184f839651afe55266c2c8420
kyoohyun <kgh2120@naver.com>
2023-01-12 21:43:49 +0900

    GM-33 JWT 처리 기능 추가

---
+++ src/main/java/com/gaaji/apigateway/filter/JwtParsingFilter.java
@@ -0,0 +1,89 @@
+package com.gaaji.apigateway.filter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+import lombok.Data;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+
+@Component
+public class JwtParsingFilter extends AbstractGatewayFilterFactory<JwtParsingFilter.Config> {
+
+    private final Environment env;
+
+    private final String secretKey;
+
+    public JwtParsingFilter(Environment env) {
+        super(Config.class);
+        this.env = env;
+        secretKey = Base64.getEncoder().encodeToString(env.getProperty("key.jwt")
+                .getBytes(StandardCharsets.UTF_8));
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) ->{
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                // 토큰이없어 ???
+            }
+
+
+            String accessToken = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0)
+                    .replace("Bearer ", "");
+
+            String authId = parseJwt(accessToken);
+
+            request = exchange.getRequest().mutate().header("authId", authId)
+                    .build();
+
+            //Custom Post Filter
+            return chain.filter(exchange.mutate().request(request).build());
+        };
+    }
+
+    private String parseJwt(String token){
+        Claims claims = Jwts.parser().setSigningKey(secretKey)
+                .parseClaimsJws(token).getBody();
+
+        if (!claims.getExpiration().after(new Date())) {
+
+        }
+
+        if (!StringUtils.hasText(claims.getSubject())) {
+
+        }
+
+        return claims.getSubject();
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String message, HttpStatus status) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(status);
+
+
+        return response.setComplete();
+    }
+    @Data
+    public static class Config {
+
+    }
+
+}
